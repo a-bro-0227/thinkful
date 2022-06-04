f0f881ac861baf2bf3cd1609de66dac2cdf113e7
Alex Brown <34726812+alexb523@users.noreply.github.com>
2022-06-04 12:32:28 -0500

    promoted folder

--- python_bits/bottles_of_beer.py
+++
@@ -1,4 +0,0 @@
-# ask user to input and store result in varibable
-bottle_count = int(input("How many bottles of beer on the wall?"))
-
-print("There are {} bottles of beer on the wall".format(bottle_count))
